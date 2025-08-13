432d41df4864b594392d479f6ef8c40ae832e9c4
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-08-13 11:24:40 +0100

    update main.py to read text file contents into a string

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print("greetings boots")
 
+
+def get_book_text(filepath):
+    """Return contents of text file as a string from the filepath
+
+    Args:
+        filepath (string): Path to text file to be read
+
+    Returns:
+        file_contents (string): Contents of file as a string
+    """
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main(filepath):
+    """Print contents of text file to console
+
+    Args:
+        filepath (string): Path to text file to be read
+
+    Returns:
+        print(string): Prints contents of file as a string
+    """
+    return print(get_book_text(filepath))
+
+
+main("books/frankenstein.txt")
