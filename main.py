30a26bd4ec0294d8ad0348c52a5ac7e73c31925b
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-08-13 12:29:11 +0100

    add character count function and print results in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
     """Return contents of text file as a string from the filepath
@@ -14,16 +14,18 @@ def get_book_text(filepath):
     return file_contents
 
 def main(filepath):
-    """Print word count of text file to console
+    """Print word and character count of text file to console
 
     Args:
         filepath (string): Path to text file to be read
 
     Returns:
-        print(string): Prints word count of file
+        print(string): Prints word and character count of file
     """
     num_words = word_count(get_book_text(filepath))
-    return print(f"{num_words} words found in the document")
-
+    character_dict = character_count(get_book_text(filepath))
+    
+    print(f"{num_words} words found in the document")
+    print(f"Character count: \n {character_dict}")
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -8,3 +8,21 @@ def word_count(filecontents):
         int: Number of words in the input string
     """
     return len(filecontents.split())
+
+def character_count(filecontents):
+    """Count the number of characters in a file
+
+    Args:
+        filecontents (string): File contents as a string
+
+    Returns:
+        dict: Dictionary containing keys for each character and the value being the count for each character
+    """
+    character_dict = {}
+    for character in filecontents.lower():
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+
+    return character_dict
