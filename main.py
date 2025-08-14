30cf530ec207f30fc11bc96a63c0b061605dece1
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-08-14 13:16:15 +0100

    add optional symbol count for text files

--- main.py
+++ main.py
@@ -15,31 +15,49 @@ def get_book_text(filepath):
     return file_contents
 
 def main(filepath):
-    """Print report of word and alphabetical character count of text file to console
+    """Print report of word and alphabetical character count of text file to console. Optional symbol count can be printed too.
 
     Args:
         filepath (string): Path to text file to be read
 
     Returns:
-        print(string): Prints report of word and alphabetical character count of text file
+        print(string): Prints report of word, alphabetical character and optional non-alphabetical character count of text file
     """
     book_text = get_book_text(filepath)
     num_words = word_count(book_text)
     character_dict = character_count(book_text)
     sorted_list = sort_dict(character_dict)
     
-    print(f"============ BOOKBOT ============ \nAnalyzing book found at {filepath}...")
-    print(f"----------- Word Count ---------- \nFound {num_words} total words")
-    print("--------- Character Count -------")
-    
-    for dict in sorted_list:
-        if dict['char'].isalpha():
-            print(f"{dict['char']}: {dict['num']}")
-            
+    print(f"============ BOOKBOT ============ \nAnalyzing book found at {filepath}...")    
+    try:
+        if sys.argv[2] == "symbol":
+            print(f"----------- Word Count ---------- \nFound {num_words} total words")
+            print("--------- Character Count -------")
+            for dict in sorted_list:
+                if dict['char'].isalpha():
+                    print(f"{dict['char']}: {dict['num']}")
+                                    
+            print("--------- Symbol Count -------")
+            for dict in sorted_list:
+                if not dict['char'].isalpha():
+                    if dict['char'] == " ":
+                        print(f"space: {dict['num']}")
+                    elif dict['char'] == "\n":
+                        print(f"newline: {dict['num']}")
+                    else:
+                        print(f"{dict['char']}: {dict['num']}")        
+        else:            
+            print("Input error: enter 'symbol' for file symbol count")
+    except:
+        print(f"----------- Word Count ---------- \nFound {num_words} total words")
+        print("--------- Character Count -------")
+        for dict in sorted_list:
+            if dict['char'].isalpha():
+                print(f"{dict['char']}: {dict['num']}")
     print("============= END ===============")
 
 if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
+    print("Usage: python3 main.py <path_to_book> [symbol]")
     sys.exit(1)
 
 main(sys.argv[1])
